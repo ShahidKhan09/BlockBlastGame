2a9008b63a176be573dba35af21ad0fb2c3ea243
Muhammad Shahid <contacttoshahidkhan@gmail.com>
2026-05-21 22:03:14 +0500

    Initial Commit

---
+++ Block Blast Puzzle/Block_Blast_PuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Block_Blast_PuzzleApp.swift
+//  Block Blast Puzzle
+//
+//  Created by Shahid's MacPro on 21/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct Block_Blast_PuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Block Blast Puzzle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Block Blast Puzzle
+//
+//  Created by Shahid's MacPro on 21/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
